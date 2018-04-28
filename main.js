bd818f62874f331c87e0807d2cd04f5bdf6d19a2
Luolei1992 <ll251537@foxmail.com>
2018-04-28 10:43:29 +0800

    ----

--- main.js
+++ main.js
@@ -6,3 +6,24 @@ var greet = require('./hello');
 var s = 'Michael';
 
 greet(s); // Hello, Michael!
+
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host: '192.168.10.105',
+    user: 'luolei',
+    password: '123',
+    database: 'test'
+});
+
+connection.connect();
+
+connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+    if (error) throw error;
+    console.log(results);
+    console.log(fields);
+    
+    console.log('The solution is: ', results[0].solution);
+});
+
+connection.end();
---
+++ mysql/config.js
@@ -0,0 +1,9 @@
+var config = {
+    host: '192.168.10.105',
+    port: 3306, // 端口号，MySQL默认3306
+    username: 'luolei',
+    password: '123',
+    database: 'test'
+};
+
+module.exports = config;
